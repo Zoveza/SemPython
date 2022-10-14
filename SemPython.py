026683e734aa77fc094489ea4b6fe7784c97386a
Anton <zoveza268@gmail.com>
2022-10-14 22:03:03 +0500

    Homework5

--- SemPython.py
+++ SemPython.py
@@ -178,10 +178,10 @@
 
 # n = int(input("Введите натуральное число: "))
 # nums = []
-# value = 1
+# candies = 1
 # for i in range(1, n + 1):
-#     nums.append(value)
-#     value *= i + 1
+#     nums.append(candies)
+#     candies *= i + 1
 # print(nums)
 
 # 3) Задайте список из n чисел последовательности (1+ (1/n))^n и выведите на экран их сумму.
@@ -377,3 +377,182 @@
 # new_lst = []
 # [new_lst.append(i) for i in lst if i not in new_lst]
 # print(f"Список неповторяющихся элементов: {new_lst}")
+
+# Лекция
+# def select(f, col):
+#  return [f(x) for x in col]
+# def where(f, col):
+#  return [x for x in col if f(x)]
+# data = '1 2 3 5 8 15 23 38'.split()
+# data = select(int, data)
+# data = where(lambda e: not e % 2, data)
+# data = list(select(lambda e: (e, e**2), data))
+# print(data)
+
+# Упрощенный вариант
+# data = '1 2 3 5 8 15 23 38'.split()
+# data = list(map(int, data))
+# data = list(filter(lambda e: not e % 2, data))
+# data = list(map(lambda e: (e, e**2), data))
+# print(data)
+
+# Семинар 5
+#1. Напишите программу, удаляющую из текста все слова, содержащие "абв"
+
+# text = 'этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+# newText = ' '.join(list(filter(lambda w: 'абв' not in w, text.split())))
+# print(newText)
+
+#2. В файле находится N натуральных чисел, записанных через пробел. Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число
+# num = list(map(int, "0 1 2 3 4 5 7 8 9 10".split(" ")))
+# for i in range(1, len(num)):
+#     if num[i]-1 != num[i-1]:
+#         print(num[i]-1)
+#         break
+# сокращенный вариант цикла for
+# print([num[i]-1 for i in range(1,len(num)) if num[i]-1 != num[i-1]][0])
+
+# Homework 5.
+
+# 1. Создайте программу для игры с конфетами человек против человека.
+
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+# a) Добавьте игру против бота
+
+# from random import randint
+
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
+
+
+# def p_print(name, k, counter, candies):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {candies} конфет.")
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# candies = int(input("Введите количество конфет на столе: "))
+# gamer = randint(0,2)
+# if gamer:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0 
+# counter2 = 0
+
+# while candies > 28:
+#     if gamer:
+#         k = input_dat(player1)
+#         counter1 += k
+#         candies -= k
+#         gamer = False
+#         p_print(player1, k, counter1, candies)
+#     else:
+#         k = input_dat(player2)
+#         counter2 += k
+#         candies -= k
+#         gamer = True
+#         p_print(player2, k, counter2, candies)
+
+# if gamer:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+
+
+# 2.Создайте программу для игры в ""Крестики-нолики"".
+
+# maps = [1,2,3,
+#         4,5,6,
+#         7,8,9]
+ 
+# victories = [[0,1,2],
+#              [3,4,5],
+#              [6,7,8],
+#              [0,3,6],
+#              [1,4,7],
+#              [2,5,8],
+#              [0,4,8],
+#              [2,4,6]]
+ 
+# def print_maps():
+#     print(maps[0], end = " ")
+#     print(maps[1], end = " ")
+#     print(maps[2])
+ 
+#     print(maps[3], end = " ")
+#     print(maps[4], end = " ")
+#     print(maps[5])
+ 
+#     print(maps[6], end = " ")
+#     print(maps[7], end = " ")
+#     print(maps[8]) 
+# def step_maps(step,symbol):
+#     ind = maps.index(step)
+#     maps[ind] = symbol
+# def get_result():
+#     win = ""
+#     for i in victories:
+#         if maps[i[0]] == "X" and maps[i[1]] == "X" and maps[i[2]] == "X":
+#             win = "X"
+#         if maps[i[0]] == "O" and maps[i[1]] == "O" and maps[i[2]] == "O":
+#             win = "O"   
+             
+#     return win
+# game_over = False
+# player1 = True
+ 
+# while game_over == False:
+#     print_maps()
+#     if player1 == True:
+#         symbol = "X"
+#         step = int(input("Игрок 1, ваш ход: "))
+#     else:
+#         symbol = "O"
+#         step = int(input("Игрок 2, ваш ход: "))
+ 
+#     step_maps(step,symbol)
+#     win = get_result()
+#     if win != "":
+#         game_over = True
+#     else:
+#         game_over = False
+#     player1 = not(player1)               
+# print_maps()
+# print("Победил", win)
+
+# 3.Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+# def coding(txt):
+#     count = 1
+#     res = ''
+#     for i in range(len(txt)-1):
+#         if txt[i] == txt[i+1]:
+#             count += 1
+#         else:
+#             res = res + str(count) + txt[i]
+#             count = 1
+#     if count > 1 or (txt[len(txt)-2] != txt[-1]):
+#         res = res + str(count) + txt[-1]
+#     return res
+
+# def decoding(txt):
+#     number = ''
+#     res = ''
+#     for i in range(len(txt)):
+#         if not txt[i].isalpha():
+#             number += txt[i]
+#         else:
+#             res = res + txt[i] * int(number)
+#             number = ''
+#     return res
+
+
+# s = input("Введите текст для кодировки: ")
+# print(f"Текст после кодировки: {coding(s)}")
+# print(f"Текст после дешифровки: {decoding(coding(s))}")
+
